0c6901d1a7941c6cb32ed2dce1043fa2b714ee08
AIDENSPC\aiden <aidenwoollard2006@gmail.com>
2023-05-12 13:26:37 +0100

    Main Menu and options menu finished.

--- Assets/Scripts/MainMenu1.cs
+++ Assets/Scripts/MainMenu1.cs
@@ -5,9 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu1 : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayTutorial()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void PlayLevel3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,37 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-using Unity.VisualScripting;
 
 public class Timer : MonoBehaviour
 {
-    public float timeRemaining = 0;
-    public bool timeIsRunning = true;
-    public TMP_Text timeText;
+    public TMP_Text timerText;
+    private float startTime;
+    private bool finished = false;
+
+    // Start is called before the first frame update
     void Start()
     {
-        timeIsRunning = true;
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timeIsRunning)
-        {
-            if(timeRemaining >= 0)
-            {
-                timeRemaining += Time.deltaTime;
-                DisplayTime(timeRemaining);
-            }
-        }
+        if (finished)
+            return;
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
     }
-    void DisplayTime(float timeToDisplay)
+
+    public void Finish()
     {
-        timeToDisplay += 1;
-        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
-        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        timeText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+        finished = true;
+        timerText.color = Color.yellow;
     }
 }
